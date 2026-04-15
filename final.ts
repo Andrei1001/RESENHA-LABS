c5e9cf206096114ecadfd00048473f4aeb700093
Jaum <jaum104239@gmail.com>
2026-04-15 13:54:52 -0300

    Update final.ts

--- final.ts
+++ final.ts
@@ -1,1 +1,14 @@
 // fazer tudo aq
+class personagem {
+    private _classe: string;
+    private _atributos = [];
+    private _nome: string;
+    private _nex: number;
+    private _protecao = 0;
+    private _inventario = [];
+    private _capacidadeInventario;
+    private _habilidades: any = {};
+    private _hpAtual = 0;
+    private _sanidadeAtual = 0;
+    private _peAtual = 0;
+}
